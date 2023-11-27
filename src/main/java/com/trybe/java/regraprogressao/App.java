5bd5fa4ccf92358703ed6f5112bdae86e1db4f52
lionelsu <lionel_suicune@hotmail.com>
2023-11-26 20:26:46 -0400

    feat: insert grade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
  */
 public class App {
 
+  private static int activityQuantity;
   private static int[] activityWeights;
+  private static int[] activityGrades;
 
   /**
    * Metodo main.
@@ -17,9 +19,10 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     String input = scanner.nextLine();
 
-    int activityQuantity = Integer.parseInt(input);
+    activityQuantity = Integer.parseInt(input);
     String[] activityNames = new String[activityQuantity];
     activityWeights = new int[activityQuantity];
+    activityGrades = new int[activityQuantity];
 
     for (int i = 1; i <= activityQuantity; i += 1) {
       System.out.println("Digite o nome da atividade " + i + ":");
@@ -29,6 +32,10 @@ public class App {
       System.out.println("Digite o peso da atividade " + i + ":");
       String weight = scanner.nextLine();
       activityWeights[i - 1] = Integer.parseInt(weight);
+
+      System.out.println("Digite a nota obtida para " + activityNames[i - 1] + ":");
+      String grade = scanner.nextLine();
+      activityGrades[i - 1] = Integer.parseInt(grade);
     }
   }
 }
